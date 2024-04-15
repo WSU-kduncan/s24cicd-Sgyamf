07af7479378c342f54c9f700a309f1fa70be5fcd
Sgyamf <143733797+Sgyamf@users.noreply.github.com>
2024-04-14 23:58:05 -0400

    deployment.sh

--- Deployment/deployment.sh
+++ Deployment/deployment.sh
@@ -1,1 +1,8 @@
 
+#!/bin/bash
+
+# Pull the latest image from DockerHub
+docker pull sgyamf/webserver:latest
+
+# Restart the Docker container (replace 'mynginx' with your container name)
+docker restart mynginx
