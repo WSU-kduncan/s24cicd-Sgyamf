98ce8b3fb9e99a22ddd88027c9959dd71de1e1be
Sgyamf <143733797+Sgyamf@users.noreply.github.com>
2024-04-14 23:55:55 -0400

    Delete Project 5/deployment.sh

--- Project 5/deployment.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/bash
-
-# Pull the latest image from DockerHub
-docker pull sgyamf/webserver:latest
-
-# Restart the Docker container (replace 'mynginx' with your container name)
-docker restart mynginx
